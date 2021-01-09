86054d33e991f87aaa4490ac3a062f1febf0394e
Kieran 'zekkxx' Anthony <zekkxx@gmail.com>
2021-01-09 13:34:29 -0600

    Setup and standard connections

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+const genBtn = document.getElementById("generate"); // document.querySelector("#generate")
+const passOutput = document.getElementById("password");
+
+console.log("Script is connected!");
